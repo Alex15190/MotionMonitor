c5fda54ed853638fb4940bb2a6e581699771333f
Alex Chekodanov <checkodanov@gmail.com>
2018-09-09 17:44:48 +0300

    Add motion monitor

--- MotionMonitor/ViewController.m
+++ MotionMonitor/ViewController.m
@@ -7,23 +7,74 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *accelerometerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gyroscopeLabel;
+
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (strong, nonatomic) NSOperationQueue *queue;
 
 @end
 
 @implementation ViewController
 
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.queue = [[NSOperationQueue alloc] init];
+    if (self.motionManager.accelerometerAvailable)
+    {
+        self.motionManager.accelerometerUpdateInterval = 1.0 / 10.0;
+        [self.motionManager startAccelerometerUpdatesToQueue:self.queue withHandler:^(CMAccelerometerData *  accelerometerData, NSError * error) {
+            NSString *labelText;
+            if (error)
+            {
+                [self.motionManager stopAccelerometerUpdates];
+                labelText = [NSString stringWithFormat:@"Accelerometer encountered error: %@", error];
+            }
+            else
+            {
+                labelText = [NSString stringWithFormat:@"Accelerometer\n---\n""x: %+.2f\ny: %+.2f\nz: %+.2f", accelerometerData.acceleration.x, accelerometerData.acceleration.y, accelerometerData.acceleration.z];
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.accelerometerLabel.text = labelText;
+            });
+        }];
+    }
+    else
+        self.accelerometerLabel.text = @"This device has no accelerometer.";
+    
+    if (self.motionManager.gyroAvailable)
+    {
+        self.motionManager.gyroUpdateInterval = 1.0/10.0;
+        [self.motionManager startGyroUpdatesToQueue:self.queue withHandler:^(CMGyroData * gyroData, NSError * error) {
+            NSString *labelText;
+            if (error)
+            {
+                [self.motionManager stopGyroUpdates];
+                labelText = [NSString stringWithFormat:@"Gyroscope encountered error: %@", error];
+            }
+            else
+            {
+                labelText = [NSString stringWithFormat:@"Gyroscope\n---\n""x: %+.2f\ny: %+.2f\nz: %+.2f",gyroData.rotationRate.x, gyroData.rotationRate.y, gyroData.rotationRate.z];
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.gyroscopeLabel.text = labelText;
+            });
+        }];
+    }
+    else
+        self.gyroscopeLabel.text = @"This device has no gyroscope";
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
